7d2af2de8c0ffde1ed7385575a52928796c23f5c
Avi <you@example.com>
2022-07-30 19:30:42 +0530

    add caart page and auth page

--- server.js
+++ server.js
@@ -4,19 +4,35 @@ const app = express();
 const path = require("path");
 const ejs = require("ejs");
 const expressEJSLayouts = require("express-ejs-layouts");
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 3000;
 
+// Assests
+
+app.use(express.static('public'))
+// Setting template engine
+
+// Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/cart",(req,res) => {
+  res.render("customers/cart")
+})
 
+app.get("/login",(req,res) => {
+  res.render("auth/login")
+})
 
-// Setting template engine
+app.get("/register",(req,res) => {
+  res.render("auth/register")
+})
+
 
 app.use(expressEJSLayouts)
 app.set('views', path.join(__dirname, './resources/views'))
 app.set('view engine', 'ejs')
+
 
 
 app.listen(PORT, () =>{
